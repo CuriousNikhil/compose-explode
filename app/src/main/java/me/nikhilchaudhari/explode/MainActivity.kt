90830da1e7c5ce07297af90f187d3bddee55143b
curiousNikhil <nikhyl777@gmail.com>
2021-09-19 11:38:12 +0530

    Improve sample code

--- app/src/main/java/me/nikhilchaudhari/explode/MainActivity.kt
+++ app/src/main/java/me/nikhilchaudhari/explode/MainActivity.kt
@@ -1,16 +1,28 @@
 package me.nikhilchaudhari.explode
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -23,7 +35,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ParticlesTheme {
                 Column(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Row(
                         modifier = Modifier
@@ -33,8 +46,11 @@ class MainActivity : ComponentActivity() {
                         horizontalArrangement = Arrangement.Center
 
                     ) {
-                        ExplodeText(text = "Click Me!", backColor = Color.Magenta)
+                        ExplodeText(text = "Click Me!", backColor = Color(0xFFe67e22))
+                        ExplodeText(text = "Click Me!", backColor = Color(0xFF9b59b6))
+                        ExplodeText(text = "Click Me!", backColor = Color(0xFF34495e))
                     }
+                    ExplodeCard()
                 }
             }
         }
@@ -44,6 +60,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ExplodeText(text: String, backColor: Color) {
+    val context = LocalContext.current
     Text(
         text = text,
         style = TextStyle(
@@ -52,9 +69,43 @@ fun ExplodeText(text: String, backColor: Color) {
             textAlign = TextAlign.Center
         ),
         modifier = Modifier
-            .explodeOnClick(color = backColor, repeatable = true)
+            .explodeOnClick(color = backColor,
+                onClick = {
+                    Toast
+                        .makeText(context, "Toast msg", Toast.LENGTH_LONG)
+                        .show()
+                })
             .padding(16.dp)
             .background(backColor, CircleShape)
             .padding(16.dp)
     )
+}
+
+
+@Composable
+fun ExplodeCard() {
+    Card(
+        modifier = Modifier
+            .explodeOnClick(color = Color(0xFFc0392b), repeatable = true)
+            .size(150.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img), contentDescription = "launcher icon", modifier =
+                Modifier.size(150.dp, 100.dp)
+            )
+
+            Text(
+                text = "Click me!", style = TextStyle(
+                    color = Color.Blue,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                ), modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
 }
